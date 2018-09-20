43b8314a83e6a9eddd93a70516af2552895ab053
Maksim Ivanov <satansdeer@gmail.com>
2018-09-20 07:26:40 +0200

    Add simple counter exercise

---
+++ simple-counter-exercise/src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import "./App.css";
+
+// Define ICounterProps interface
+// should include the "state" field of type "number"
+
+// Set proper visibility levels for class properties
+// public/protected/private
+
+class App extends React.Component<{}, ICounterProps> {
+  state = { counter: 0 };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="counter">{this.state.counter}</div>
+        <button onClick={this.increment}>+1</button>
+        <button onClick={this.decrement}>-1</button>
+      </div>
+    );
+  }
+
+  increment = () => {
+    this.setState(state => {
+      return { counter: state.counter + 1 };
+    });
+  };
+
+  decrement = () => {
+    this.setState(state => {
+      return { counter: state.counter - 1 };
+    });
+  };
+}
+
+export default App;
